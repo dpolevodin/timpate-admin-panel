1ec0d19327600edc4e011b2d10e47d4923f1b005
dpolevodin <dpolevodin@gmail.com>
2026-07-01 18:15:02 +0600

    feat: added hltn qr

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Layout } from "./components/layout";
 import { GenerateQr } from "./components/GenerateQr";
 import { OrganizationLogoContainer } from "./components/OrganizationLogoContainer";
+import { GenerateHltnQr } from "./components/GenerateHltnQr";
+
+import "@fontsource/playfair-display/500.css";
+import "@fontsource/inter/900.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
 
 export const App = () => {
   return (
     <Layout>
       <GenerateQr />
       <OrganizationLogoContainer />
+      <GenerateHltnQr />
     </Layout>
   );
 };
